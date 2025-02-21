1724424f6af63d5cc2380504210e17efd5720c2e
ganeshtambekar <ganesh.tambekar124@gmail.com>
2025-02-21 10:36:58 +0530

    path changed

--- server/index.js
+++ server/index.js
@@ -48,42 +48,42 @@
 
 
 const express = require("express");
-const cors = require("cors");
-const connectDB = require("./config/db");
-require("dotenv").config();
-const authRoutes = require("./routes/auth");
-const emailRoutes = require("./routes/email"); 
-const courseRoutes=require("./routes/courses");
-const { errorHandler } = require("./utils/errorhandler");
+// const cors = require("cors");
+// const connectDB = require("./config/db");
+// require("dotenv").config();
+// const authRoutes = require("./routes/auth");
+// const emailRoutes = require("./routes/email"); 
+// const courseRoutes=require("./routes/courses");
+// const { errorHandler } = require("./utils/errorhandler");
 const router = express.Router();
 
 
 const app = express();
 
-(async () => {
-  try {
-    await connectDB();
-    console.log("Database connected successfully.");
-  } catch (err) {
-    console.error("Database connection failed:", err);
-    process.exit(1);
-  }
-})();
+// (async () => {
+//   try {
+//     await connectDB();
+//     console.log("Database connected successfully.");
+//   } catch (err) {
+//     console.error("Database connection failed:", err);
+//     process.exit(1);
+//   }
+// })();
 
-// Middleware
-app.use(express.json());
+// // Middleware
+// app.use(express.json());
 
-// CORS Configuration
-const allowedOrigins = [
-  process.env.FRONTEND_URL || "http://localhost:3000"
-];
+// // CORS Configuration
+// const allowedOrigins = [
+//   process.env.FRONTEND_URL || "http://localhost:3000"
+// ];
 
-app.use(cors());
+// app.use(cors());
 
-// Routes
-app.use("/api", authRoutes);
-app.use("/api", emailRoutes); 
-app.use("/api",courseRoutes);
+// // Routes
+// app.use("/api", authRoutes);
+// app.use("/api", emailRoutes); 
+// app.use("/api",courseRoutes);
 router.get("/", (req, res) => {
   res.send("Welcome");
 });
@@ -92,7 +92,9 @@ router.get("/", (req, res) => {
 app.use("/", router);
 
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+module.exports = app;
