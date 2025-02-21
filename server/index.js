410a506f45d49bcd16a182b7ee818301189e5e6b
ganeshtambekar <ganesh.tambekar124@gmail.com>
2025-02-21 11:05:39 +0530

    path changed

--- server/index.js
+++ server/index.js
@@ -1,51 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const connectDB = require('./config/db');
-// require('dotenv').config();
-// const authRoutes = require('./routes/auth');
-// const { errorHandler } = require('./utils/errorhandler');
-
-// const app = express();
-
-// // Connect to Database
-// (async () => {
-//   try {
-//     await connectDB();
-//     console.log('Database connected successfully.');
-//   } catch (err) {
-//     console.error('Database connection failed:', err);
-//     process.exit(1);
-//   }
-// })();
-
-// // Middleware
-// app.use(express.json());
-
-// // CORS Configuration
-// const allowedOrigins = [
-//   process.env.FRONTEND_URL || 'http://localhost:3000',
-//   'http://localhost:5173', // Vite (if needed)
-// ];
-
-// app.use(
-//   cors()
-// );
-
-// // Handle preflight requests
-// // app.options('*', cors());
-
-// // Routes
-// app.use('/api', authRoutes);
-
-// // Error Handler (Always at the End)
-// app.use(errorHandler);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
-
-
-
 
 const express = require("express");
 const cors = require("cors");
@@ -53,8 +5,8 @@ const connectDB = require("./config/db");
 require("dotenv").config();
 const authRoutes = require("./routes/auth");
 const emailRoutes = require("./routes/email"); 
-const courseRoutes=require("./routes/courses");
-// const {errorHandler} = require("./utils/errorhandler");
+//const courseRoutes=require("./routes/courses");
+const {errorHandler} = require("./utils/errorhandler");
 const router = express.Router();
 
 
@@ -83,7 +35,7 @@ app.use(express.json());
 // // Routes
 app.use("/api", authRoutes);
 app.use("/api", emailRoutes); 
-app.use("/api",courseRoutes);
+//app.use("/api",courseRoutes);
 router.get("/", (req, res) => {
   res.send("Welcome");
 });
